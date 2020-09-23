c5b3ba3cd6f649eee48289b02681a688244afa46
Campari229 <KennyKoin@gmail.com>
2020-09-23 20:18:46 +0300

    Updates

---
+++ precache-manifest.29fdb216ee82b7c52ff09f9431c7a33b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae71264f87cb1bdbaa72c258410c3c9",
+    "url": "/abz-tt/index.html"
+  },
+  {
+    "revision": "9b221228cc42eb9c825d",
+    "url": "/abz-tt/static/css/main.10762867.chunk.css"
+  },
+  {
+    "revision": "36c6672cd2f92f66a264",
+    "url": "/abz-tt/static/js/2.2e1e5a63.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/abz-tt/static/js/2.2e1e5a63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b221228cc42eb9c825d",
+    "url": "/abz-tt/static/js/main.a90bf71b.chunk.js"
+  },
+  {
+    "revision": "2c09b00fbf76e9c8a959",
+    "url": "/abz-tt/static/js/runtime-main.b1e753c0.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/abz-tt/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/abz-tt/static/media/banner-photo.f5443d74.jpg"
+  }
+]);
